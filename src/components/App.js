e8c1ce6cd32550d537043580e7cd6ee530fa7f01
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-13 12:14:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
 import React from "react";
 import './../styles/App.css';
+import Todo from "./Todo.jsx";
 
 const App = () => {
+  const todolist = [
+    {
+      id: "1",
+      task: "Learn React",
+      completed: "false",
+    },
+    {
+      id: "2",
+      task: "Build a React App",
+      completed: "false",
+    },
+    {
+      id: "3",
+      task: "Deploy a React App",
+      completed: "false",
+    },
+  ];
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ul>
+        {todolist.map((ele) => (
+          <Todo key={ele.id} isCompleted={ele.completed} task={ele.task} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
